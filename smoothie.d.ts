304a336a65258de71bfead0f11c95490ebc1c381
WofWca <wofwca@protonmail.com>
2020-11-06 17:45:03 +0800

    chore: bump version to 1.36.0

--- smoothie.d.ts
+++ smoothie.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Smoothie Charts 1.35
+// Type definitions for Smoothie Charts 1.36
 // Project: https://github.com/joewalnes/smoothie
 // Definitions by: Drew Noakes <https://drewnoakes.com>
 //                 Mike H. Hawley <https://github.com/mikehhawley>
